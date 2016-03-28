82185d2175423825f409823ab6543f9fa22f0473
Igor Derkach <gosha753951@gmail.com>
2016-03-28 18:49:13 +0300

    Firmware

---
+++ firmware/config_application.h
@@ -0,0 +1,15 @@
+/*----------------
+---- Hardware ----
+------------------*/
+
+
+
+/*----------------
+----- Tasks ------
+------------------*/
+
+#define	CN_00	Display
+#define	CN_01	Input
+#define CN_02	ADC
+#define CN_04	TimeTemp
+
---
+++ firmware/main.c
@@ -0,0 +1,17 @@
+#include "femtoos_code.h"
+
+int main()
+{
+
+}
+
+#if (preTaskDefined(Display))
+
+void appLoop_Display(void)
+{
+	while(true) {
+		
+	}
+}
+
+#endif
