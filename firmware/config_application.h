6d7b8bce5dc5fae04c377eb0eae37d655ead55ad
Igor Derkach <gosha753951@gmail.com>
2016-03-31 09:21:22 +0300

    Firmware

--- firmware/config_application.h
+++ firmware/config_application.h
@@ -5,11 +5,17 @@
 ---- Hardware ----
 ------------------*/
 
+#define INPUT_PORT	PORTC;
+#define INPUT_PIN	PINC;
+#define INPUT_DDR	DDRC;
 #define	VOLTAGE_PIN	PC7;
 #define CHARGE_PIN	PC0;
-#define BUTTON_PIN	PB0;
+#define BUTTON_PIN	PC6;
 
 //7-seg indicator
+#define DISP_PORT	PORTD;
+#define DISP_PIN	PIND;
+#define DISP_DDR	DDRD;
 #define DISP_A		PD0;
 #define DISP_B		PD1;
 #define DISP_C		PD2;
@@ -18,6 +24,9 @@
 #define DISP_F		PD5;
 #define DISP_G		PD6;
 // digits
+#define DISP_D_PORT	PORTB;
+#define DISP_D_PIN	PINB;
+#define DISP_D_DDR	DDRB;
 #define DISP_D0		PB1;
 #define DISP_D1		PB2;
 #define DISP_D2		PB3;
@@ -25,7 +34,20 @@
 #define DISP_D4		PB5;
 
 
-
+/*----------------
+---- Software ----
+------------------*/
+/* Digits patters depending on DISP_X macroses */
+#define	DIG_PATTERN_0	_BV(DISP_A)|_BV(DISP_B)|_BV(DISP_C)|_BV(DISP_D)|_BV(DISP_E)|_BV(DISP_F)
+#define	DIG_PATTERN_1	_BV(DISP_B)|_BV(DISP_C)
+#define	DIG_PATTERN_2	_BV(DISP_A)|_BV(DISP_B)|_BV(DISP_G)|_BV(DISP_E)|_BV(DISP_D)
+#define	DIG_PATTERN_3	_BV(DISP_A)|_BV(DISP_B)|_BV(DISP_C)|_BV(DISP_D)|_BV(DISP_G)
+#define DIG_PATTERN_4	_BV(DISP_B)|_BV(DISP_C)|_BV(DISP_F)|_BV(DISP_G)
+#define	DIG_PATTERN_5	_BV(DISP_A)|_BV(DISP_F)|_BV(DISP_G)|_BV(DISP_C)|_BV(DISP_D)
+#define	DIG_PATTERN_6	_BV(DISP_C)|_BV(DISP_D)|_BV(DISP_E)|_BV(DISP_F)|_BV(DISP_G)
+#define	DIG_PATTERN_7	_BV(DISP_A)|_BV(DISP_B)|_BV(DISP_C)
+#define	DIG_PATTERN_8	_BV(DISP_A)|_BV(DISP_B)|_BV(DISP_C)|_BV(DISP_D)|_BV(DISP_E)|_BV(DISP_F)|_BV(DISP_G)
+#define	DIG_PATTERN_9	_BV(DISP_A)|_BV(DISP_B)|_BV(DISP_C)|_BV(DISP_F)|_BV(DISP_G)
 
 
 /*----------------
