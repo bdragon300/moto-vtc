e8ff3f5011d88762321e9dda63fbb021b98f731a
Igor Derkach <gosha753951@gmail.com>
2016-05-15 10:24:47 +0300

    Write business logic

---
+++ firmware/business_logic.c
@@ -0,0 +1,126 @@
+/*
+ * business_logic.c
+ *
+ *  Created on: May 14, 2016
+ *      Author: gosha
+ */
+
+/*
+ * === VARIABLES ===
+ */
+
+/*
+ * Current mode in which device is now
+ */
+extern Device_mode_t device_mode = {0};
+
+/*
+ * Data that was fetched from all sources
+ */
+struct {
+	ds1629_Temp_t temp;
+	ds1629_Time_t clock;
+	uint8_t volt;
+}
+fetched_data = {0};
+
+
+/*
+ * === FUNCTIONS ===
+ */
+
+void
+business_init(void)
+{
+	device_mode.display_mode = INIT;
+	fetch_all_data();
+}
+
+void
+button_click(void)
+{
+	if (device_mode.forbid_button)
+		return;
+
+	if (device_mode.settings.settings_mode == NONE) {
+		++device_mode.display_mode;
+
+		if (device_mode.display_mode > TEMP) {
+			device_mode.display_mode = TIME;
+		}
+
+		switch (device_mode.display_mode) {
+			case TIME:
+				show_time(fetched_data.clock);
+				break;
+			case VOLT:
+				show_volt(fetched_data.volt);
+				break;
+			case TEMP:
+				show_temp(fetched_data.temp);
+				break;
+		}
+	}
+	else if (device_mode.settings.settings_mode == TIME) {
+		++device_mode.settings.time_settings.current_digit;
+
+		if (device_mode.settings.time_settings.current_digit > 3) {
+			device_mode.settings.settings_mode == NONE;
+			ds1629_write_clock(DS1629_ADDR, device_mode.settings.time_settings.new_time);
+			fetched_data.clock = device_mode.settings.time_settings.new_time;
+			//release semaphore
+		}
+	}
+}
+
+void
+button_hold(void)
+{
+	if (device_mode.forbid_button)
+		return;
+
+	if (device_mode.settings.settings_mode == NONE) {
+		//get semaphore
+		device_mode.settings.settings_mode == TIME;
+		device_mode.settings.time_settings.new_time = fetched_data.clock;
+		device_mode.settings.time_settings.current_digit = 0;
+
+		device_mode.forbid_button = 1; //User must release button after entering settings
+	}
+	else if (device_mode.settings.settings_mode == TIME) {
+		switch (device_mode.settings.time_settings.current_digit) {
+			case 0:
+				_time_increment_dechour(device_mode.settings.time_settings.new_time);
+				break;
+			case 1:
+				_time_increment_onehour(device_mode.settings.time_settings.new_time);
+				break;
+			case 2:
+				_time_increment_decmin(device_mode.settings.time_settings.new_time);
+				break;
+			case 3:
+				_time_increment_onemin(device_mode.settings.time_settings.new_time);
+				break;
+		}
+		show_time(device_mode.settings.time_settings.new_time);
+	}
+}
+
+void
+button_release(void)
+{
+	if (device_mode.settings.settings_mode == TIME) {
+		device_mode.forbid_button = 0;
+	}
+
+	if (device_mode.forbid_button)
+		return;
+}
+
+void
+fetch_all_data(void)
+{
+	fetched_data.temp = ds1629_read_temp(DS1629_ADDR);
+	fetched_data.clock = ds1629_read_clock(DS1629_ADDR);
+	fetched_data.volt = get_volts();
+}
---
+++ firmware/business_logic.h
@@ -0,0 +1,123 @@
+/*
+ * business_logic.h
+ *
+ *  Created on: May 14, 2016
+ *      Author: gosha
+ */
+
+#ifndef BUSINESS_LOGIC_H_
+#define BUSINESS_LOGIC_H_
+
+#include "config_application.h"
+#include "ds1629/ds1629.h"
+
+/*
+ * === TYPES ===
+ */
+
+/*
+ * Current display mode
+ */
+typedef enum {TIME, VOLT, TEMP, INIT} Display_mode_t;
+
+/*
+ * Settings
+ */
+typedef enum {NONE, TIME} Settings_mode_t;
+typedef struct {
+	Settings_mode_t settings_mode;
+	struct {
+		uint8_t current_digit;
+		ds1629_Time_t new_time;
+	} time_settings;
+} Device_settings_t;
+
+/*
+ * Device mode
+ */
+typedef struct {
+	Display_mode_t display_mode;
+	Device_settings_t settings;
+	uint8_t forbid_button;
+} Device_mode_t;
+
+/*
+ * === FUNCTIONS ===
+ */
+
+/*
+ * Initialization
+ */
+void
+business_init(void);
+
+/*
+ * Calls when button clicked
+ */
+void
+button_click(void);
+
+/*
+ * Calls when button is started hold and repeatedly while it is pressing
+ */
+void
+button_hold(void);
+
+/*
+ * Calls when button released
+ */
+void
+button_release(void);
+
+/*
+ * Fetch and updates actual data from all devices
+ */
+void
+fetch_all_data(void);
+
+/*
+ * Returns mask of half-brigthness digits. 1 is shadowed.
+ */
+uint8_t
+get_digits_shadow_mask(void)
+{
+	if (device_mode.settings.settings_mode == TIME)
+		return ~(0x80 >> device_mode.settings.time_settings.current_digit);
+	return 0;
+}
+
+/*
+ * === UTILITIES ===
+ */
+static void
+_time_increment_decmin(ds1629_Time_t* time)
+{
+	time->minutes = (time->minutes > 49) ? time->minutes % 10 : time->minutes + 10;
+}
+static void
+_time_increment_onemin(ds1629_Time_t* time)
+{
+	if (++time->minutes % 10 == 0)
+		time->minutes -= 10;
+}
+static void
+_time_increment_dechour(ds1629_Time_t* time)
+{
+	time->hours = (time->hours > 19) ? time->hours % 10 : time->hours + 10;
+
+	if (time->hours > 23)
+		time->hours = 23;
+}
+static void
+_time_increment_onehour(ds1629_Time_t* time)
+{
+	if (++time->hours % 10 == 0)
+		time->hours -= 10;
+	else
+		++time->hours;
+
+	if (time->hours > 23)
+		time->hours = 20;
+}
+
+#endif /* BUSINESS_LOGIC_H_ */
